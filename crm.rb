4df232d71f6363c6772e7509dccc85af45460510
jmartow <josh.martow@gmail.com>
2014-06-10 23:06:58 -0400

    added 'new', 'id', 'id/edit'

--- crm.rb
+++ crm.rb
@@ -1,10 +1,24 @@
 require 'sinatra'
 
-get '/contacts' do
+get '/contacts/new' do
   @crm_app_name = "My CRM"
   erb :contacts
 end
 
+get '/contacts/:id/edit' do
+  @crm_app_name = "My CRM"
+  erb :contacts
+end
+
+get '/contacts/:id' do
+  @crm_app_name = "My CRM"
+  erb :contacts
+end
+
+get '/contacts' do
+  @crm_app_name = "My CRM"
+  erb :contacts
+end
 
 get '/' do
   @crm_app_name = "My CRM"
