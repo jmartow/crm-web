69f6082656714f51893333760ee59291f8d84db7
jmartow <josh.martow@gmail.com>
2014-06-10 23:37:48 -0400

    contacts.erb and /contacts route working

--- crm.rb
+++ crm.rb
@@ -22,7 +22,10 @@ get '/contacts/:id' do
 end
 
 get '/contacts' do
-  @crm_app_name = "My CRM"
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
   erb :contacts
 end
 
